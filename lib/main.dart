fc416520c93028402a2c7ad4f61500a8ef665ac1
NicholasManurung <agust.nico@gmail.com>
2021-09-02 19:59:37 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: WebViewExample(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Builder(builder: (BuildContext context) {
+          return WebView(
+            initialUrl: 'https://indonesianstatistics.adriel-creation.tech/',
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+            },
+            gestureNavigationEnabled: true,
+          );
+        }),
+      ),
+    );
+  }
+}
